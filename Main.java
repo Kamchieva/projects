f917f2fea883526f56dad05eefec0a566af36e9c
Aidan <150118075+Kamchieva@users.noreply.github.com>
2024-12-25 16:13:14 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,23 @@
-import java.util.HashMap;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Node {
+    int data ;
+    Node next ;
+    Node(int data)  {
+        this.data = data ;
+    }
+    int countNodes(Node head) {
+ int count = 1;
+ Node current = head;
 
-public class Main {
-    public static void main(String[] args) {
-        HashMap<String, Integer> empIds = new HashMap<>() ;
-        empIds.put("John", 12345) ;
-        empIds.put("Carl", 54321) ;
-        empIds.put("Jerry", 455664455) ;
+ while(current.next != null) {
+     current = current . next ;
+     count += 1;
 
-        System.out.println(empIds) ;
-        System.out.println(empIds.containsValue(12345)) ;
-        empIds.put("John", 3333) ;
-        empIds.replace("Karen", 777) ;
-
-        empIds.putIfAbsent("Steve",222);
-        empIds.remove("Steve");
-        System.out.println(empIds) ;
+ }
+        return count;
     }
 }
+    public static void main(String[] args) {
+
+    }
