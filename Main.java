e03678aa306c7ff1a355e85b142ab58264598c58
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-01-04 13:06:25 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
-package Random;
+package week3;
 
-import java.util.Random;
+import java.util.HashMap;
 
 public class Main {
-
     public static void main(String[] args) {
-        Random random = new Random();
 
-        //int x = random.nextInt(6) + 1;
-        //double y = random.nextDouble();
-        boolean z = random.nextBoolean();
+        HashMap<String,Integer> empIds = new HashMap<>();
+        empIds.put("John", 1234);
+        empIds.put("Jerry", 234);
+        empIds.put("Karen", 345);
 
-        System.out.println(z);
+        System.out.println(empIds);
+
     }
 }
