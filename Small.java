3520d195fb3910ddd09b6d3d5777bfb4c483b650
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-01-06 15:40:07 -0600

    Add files via upload

---
+++ Small.java
@@ -0,0 +1,20 @@
+package Smalled;
+import java.util.Arrays;
+
+public class Small {
+    public static void main(String[] args) {
+
+  int[] A = {5, 7, 8, 88, 0, 23};
+  int n = A.length;
+
+  for(int i = 0; i<= n-1; i++ ){
+      int minIndex = A[i];
+      
+
+
+  }
+    }
+
+        }
+
+
