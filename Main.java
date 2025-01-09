f61813b46501aeb0a07ec934510de8bd94ee659f
Aidan <150118075+Kamchieva@users.noreply.github.com>
2025-01-08 20:14:19 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
-import java.util.Scanner;
+import java.util.Hashtable;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
+        Hashtable<String, String> table = new Hashtable<>(21);
+        table.put("100", "Spongebob");
+        table.put("123", "Patrick");
+        table.put("321", "Sandy");
+        table.put("555", "Squidward");
+        table.put("777", "Gary");
+        //table.remove(777) ;
+        for (String key : table.keySet()) {
+            System.out.println(key.hashCode() % 10  +  "\t" +key + "\t" + table.get(key)) ;
 
-        System.out.println("Hello" + name);
-
+        }
     }
 }
